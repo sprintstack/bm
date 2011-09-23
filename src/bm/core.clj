3d6d4b116e19c5a969fe06241e485cda2d368bad
Chris Mowforth <chris@mowforth.com>
2011-09-23 01:51:36 +0100

    Proper agent spawning

--- src/bm/core.clj
+++ src/bm/core.clj
@@ -1,12 +1,34 @@
 (ns bm.core
-  (:use [clojure.contrib.http.agent])
+  (:import (java.io InputStream InputStreamReader BufferedReader)
+           (java.net URL HttpURLConnection))
   (:gen-class))
 
-(defn benchmark [body]
+(defmacro benchmark [body]
   `(let [now# ~(System/nanoTime)]
      (println "Benchmarking...")
      ~body
-     (println "That took: " (- (System/nanoTime now)))))
+     (println "That took: " (/ (- ~(System/nanoTime) now#) 1E6) "ns")))
+
+
+(defn fetch [url]
+  "synchronously fetches a url, returns same url if successful
+  to preserve agent state"
+    (let [conn (.openConnection (URL. url))]
+      (.setRequestMethod conn "GET")
+      (.connect conn)
+      (with-open [stream (BufferedReader.
+                          (InputStreamReader. (.getInputStream conn)))]
+        (.toString (reduce #(.append %1 %2)
+                           (StringBuffer.) (line-seq stream)))))
+    url)
+
+(defn spawn-agents [url c n]
+  "url being the url to hit, c being the number of concurrent connections,
+  n being the number of requests per connection"
+  (let [agents (take c (cycle [(agent url)]))]
+    (doseq [a agents]
+      (dotimes [x n] (send-off a fetch)))
+    (apply await agents)))
 
 (defn -main [& args]
   (println "TODO"))
